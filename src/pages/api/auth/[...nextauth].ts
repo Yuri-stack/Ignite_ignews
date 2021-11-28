f7bdce58c7ae311a62c66bd3ac749fc15c0de866
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-11-28 15:26:16 -0300

    Feat.: Saving user to database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,33 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET
     }),
-    // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(  // Usando o Fauna, criamos uma query
+          // A query criada, acessa as Collections users, e salva o email no campo data
+          q.Create(q.Collection('users'), { data: { email } })
+        )
+        return true
+
+      } catch (error) {
+        console.log(error)
+
+        return false
+      }
+    }
+  }
 })
