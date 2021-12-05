a469c089482925ed876b741e9565d008837e2ca5
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-12-05 17:35:33 -0300

    Feat.: Redirecting to Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { useSession, signIn } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
@@ -8,14 +11,24 @@ interface SubscribeButtonProps{
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
     const { data: session } = useSession()
 
-    function handleSubscribe(){
+    async function handleSubscribe(){
         if(!session){
             signIn('github')
             return
         }
 
         // Criação da Checkout Session do Stripe
-        
+        try {
+            const response = await api.post('/subscribe')
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId })
+
+        } catch (error) {
+            alert(error.message)
+        }
 
     }
 
