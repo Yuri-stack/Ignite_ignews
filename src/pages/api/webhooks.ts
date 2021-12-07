ed2da429b37bf52c499451fac558bb8255615c2b
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-12-07 20:51:31 -0300

    Feat.: Listening to Stripe Events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,62 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    console.log("Evento Recebido")
+import { stripe } from "../../services/stripe";
 
-    res.status(200).json({ ok: true })
+// Função para pegar todas as info retornadas pouco a pouco, e transformar em um Objeto Buffer 
+async function buffer(readable: Readable){
+    const chunks = []
+
+    for await(const chunk of readable){
+        chunks.push(
+            typeof chunk === "string" ? Buffer.from(chunk) : chunk
+        )
+    }
+
+    return Buffer.concat(chunks)
+}
+
+/*Por padrão, o Next espera que as Req. sejam JSON e afins, como essa Req. é uma stream, 
+desabilitamos o comportamento padrão através dessa config.*/
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+// Eventos que queremos "ouvir"/atender do Stripe
+const relevantEvents = new Set(["checkout.session.completed"])
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method == 'POST') {
+
+        // Pega os dados retornados pela aplicação Stripe
+        const buf = await buffer(req)
+
+        // Pega a chave secreta que o Stripe envia, validando que foi a aplicação Stripe que fez a requição
+        const secret = req.headers['stripe-signature'] 
+
+        let event: Stripe.Event
+
+        try{
+            // Verificando se os parametros batem, e construindo o Objeto de evento
+            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)           
+        } catch (err) {
+            return res.status(400).send(`Webhook error: ${err.message}`)
+        }
+
+        // Pegando o tipo do event retornado pela Aplicação do Stripe ao validar uma compra
+        const { type } = event
+
+        if(relevantEvents.has(type)){
+            console.log("Evento Recebido", event)
+        }
+
+        res.json({ ok: true })
+
+    } else {
+        res.setHeader('Allow', 'POST')
+        res.status(405).end('Method not allowed')
+    }
 }
