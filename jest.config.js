5bbbfbc720c01d6a5ce6eafb91bb3da44a5101dc
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-06-20 21:15:41 -0300

    Config.: Configurando Testing Library

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testIgnorePatterns: ["/node_modules/", "/.next/"],   // Pastas a serem ignoradas
+    setupFilesAfterEnv: [
+        "<rootDir>src/tests/setupTests.ts"
+    ],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+    },
+    testEnvironment: 'jsdom'
+}
